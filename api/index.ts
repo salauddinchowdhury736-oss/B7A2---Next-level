6e9c5972a1c194c420adfeacd74bb159e1d90de8
Developer <dev@example.com>
2026-06-08 22:59:47 +0600

    Remove old TypeScript API handler

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'http';
-import app from '../src/app';
-
-export default (req: IncomingMessage, res: ServerResponse) => {
-  return app(req, res);
-};
